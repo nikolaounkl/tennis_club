7e467d3ee521cf20ebdf694d6a9b51f6a128d341
nikolaounkl <nikolaounkl@gmail.com>
2019-04-19 20:27:53 +0200

    Updated app.js template.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="box">
-          <h1 className="title">New React Project Template</h1>
-          <div className="box">
-            <h2 class="subtitle">Font Awesome</h2>
-            <h2 class="subtitle">Bulma</h2>
-            <h2 class="subtitle">Bootstrap 4</h2>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="box">
+                    <h1 className="title">New React Project Template</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
