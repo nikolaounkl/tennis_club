51d90a8c158e91fb40bba3bdf95dafe86c4f58fe
nikolaounkl <nikolaounkl@gmail.com>
2019-04-23 22:17:20 +0200

    Handled ouside navbar click.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,25 +1,61 @@
 import React, { Component } from "react";
 
 export default class Footer extends Component {
-    openPDF = () => {
-
+    impresium = () => {
+        window.open(
+            "http://" + window.location.hostname + "/images/impresium.pdf"
+        );
+    };
+    Datenschutz = () => {
+        window.open(
+            "http://" + window.location.hostname + "/images/datenschutz.pdf"
+        );
+    };
+    agb = () => {
+        window.open(
+            "http://" + window.location.hostname + "/images/agb_training.pdf"
+        );
     };
     render() {
         return (
-            <nav className="level navbar is-black is-fixed-bottom">
+            <nav className="level navbar is-black is-fixed-bottom is-hidden-mobile">
                 <div className="level-item has-text-centered">
-                    <p className="link has-text-white ">Copyright © 2018 T.S.V. Marzahner Füchse e.V.</p>
+                    <p className="link has-text-white is-size-7">
+                        Copyright © 2018 T.S.V. Marzahner Füchse e.V.
+                    </p>
+                </div>
+                <div className="level-item">
+                    <button
+                        className="link button is-black has-text-danger"
+                        onClick={this.impresium}
+                    >
+                        <span className="is-size-7">Impresium</span>
+                    </button>
+                </div>
+                <div className="level-item">
+                    <button
+                        className="link button is-black has-text-danger"
+                        onClick={this.Datenschutz}
+                    >
+                        <span className="is-size-7">Datenschutz</span>
+                    </button>
+                </div>
+                <div className="level-item">
+                    <button
+                        className="link button is-black has-text-danger"
+                        onClick={this.agb}
+                    >
+                        <span className="is-size-7">AGB Training</span>
+                    </button>
                 </div>
                 <div className="level-item">
                     <a
-                        className="link has-text-danger"
+                        className="link button is-black has-text-danger"
                         href="http://www.nikolaounikolaos.com/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <span className="has-text-weight-semibold">
-                            Created by NN
-                        </span>
+                        <span className="is-size-7">Created by NN</span>
                     </a>
                 </div>
             </nav>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,106 +5,125 @@ export default class Navbar extends Component {
     state = {
         mobileMenu: false
     };
+
+    componentWillMount() {
+        document.addEventListener("mousedown", this.handleOutsideClick, false);
+    }
+    componentWillUnmount() {
+        document.removeEventListener(
+            "mousedown",
+            this.handleOutsideClick,
+            false
+        );
+    }
     toggleMenu = () => {
         this.setState(prevState => ({
             mobileMenu: !prevState.mobileMenu
         }));
     };
+    handleOutsideClick = e => {
+        // ignore clicks on the component itself
+        if (this.node.contains(e.target)) {
+            return;
+        }
+
+        this.toggleMenu();
+    };
     render() {
         return (
-            <div>
-                <nav
-                    className="navbar is-white is-fixed-top"
-                    role="navigation"
-                    aria-label="main navigation"
-                >
-                    <div className="navbar-brand">
-                        <NavLink
-                            to="/"
-                            className="navbar-item has-text-danger is-size-5"
-                        >
-                            T.S.V. Marzahner Füchse e.V.
-                        </NavLink>
+            <nav
+                ref={node => (this.node = node)}
+                className="navbar is-white is-fixed-top"
+                role="navigation"
+                aria-label="main navigation"
+            >
+                <div className="navbar-brand">
+                    <NavLink
+                        to="/"
+                        className="navbar-item has-text-danger is-size-5"
+                        onClick={this.state.mobileMenu ? this.toggleMenu : null}
+                    >
+                        T.S.V. Marzahner Füchse e.V.
+                    </NavLink>
 
-                        <span
-                            role="button"
-                            className={
-                                this.state.mobileMenu
-                                    ? "navbar-burger burger is-active"
-                                    : "navbar-burger burger"
-                            }
-                            aria-label="menu"
-                            aria-expanded="false"
-                            data-target="navbarBasicExample"
-                            onClick={this.toggleMenu}
-                        >
-                            <span aria-hidden="true" />
-                            <span aria-hidden="true" />
-                            <span aria-hidden="true" />
-                        </span>
-                    </div>
-
-                    <div
-                        id="navbarBasicExample"
+                    <span
+                        role="button"
                         className={
                             this.state.mobileMenu
-                                ? "navbar-menu is-active"
-                                : "navbar-menu"
+                                ? "navbar-burger burger is-active"
+                                : "navbar-burger burger"
                         }
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbarBasicExample"
+                        onClick={this.toggleMenu}
                     >
-                        <div className="navbar-end">
-                            <NavLink
-                                to="/verein"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Verein
-                            </NavLink>
-                            <NavLink
-                                to="/vorstand"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Vorstand
-                            </NavLink>
-                            <NavLink
-                                to="/training"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Training
-                            </NavLink>
-                            <NavLink
-                                to="/platzbelegung"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Platzbelegung
-                            </NavLink>
-                            <NavLink
-                                to="/sports"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Sports
-                            </NavLink>
-                            <NavLink
-                                to="/lageplan"
-                                className="navbar-item"
-                                activeClassName="is-active"
-                                onClick={this.toggleMenu}
-                            >
-                                Lageplan
-                            </NavLink>
-                        </div>
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                    </span>
+                </div>
+
+                <div
+                    id="navbarBasicExample"
+                    className={
+                        this.state.mobileMenu
+                            ? "navbar-menu is-active"
+                            : "navbar-menu"
+                    }
+                >
+                    <div className="navbar-end">
+                        <NavLink
+                            to="/verein"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Verein
+                        </NavLink>
+                        <NavLink
+                            to="/vorstand"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Vorstand
+                        </NavLink>
+                        <NavLink
+                            to="/training"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Training
+                        </NavLink>
+                        <NavLink
+                            to="/platzbelegung"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Platzbelegung
+                        </NavLink>
+                        <NavLink
+                            to="/sports"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Sports
+                        </NavLink>
+                        <NavLink
+                            to="/lageplan"
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            onClick={this.toggleMenu}
+                        >
+                            Lageplan
+                        </NavLink>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         );
     }
 }
