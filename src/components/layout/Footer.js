3d5cad2391f95d55d0eb2ccb2ab1e7a8c792e5ab
nikolaounkl <nikos.nikolaou.dev@gmail.com>
2020-10-10 17:15:13 +0200

    Updated vorstand page

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,63 +1,58 @@
 import React, { Component } from "react";
 
 export default class Footer extends Component {
-    impresium = () => {
-        window.open(
-            "http://www.tennis-fuechse-marzahn.de/images/verein-impressum.pdf"
-        );
-    };
-    Datenschutz = () => {
-        window.open(
-            "http://www.tennis-fuechse-marzahn.de/images/datenschutz.pdf"
-        );
-    };
-    agb = () => {
-        window.open(
-            "http://www.tennis-fuechse-marzahn.de/images/agb_training.pdf"
-        );
-    };
-    render() {
-        return (
-            <nav className="level navbar is-black is-fixed-bottom is-hidden-mobile">
-                <div className="level-item has-text-centered">
-                    <p className="link has-text-white is-size-7">
-                        Copyright © 2018 T.S.V. Marzahner Füchse e.V. | Created
-                        by
-                        <a
-                            className="link has-text-danger"
-                            href="http://www.nikolaounikolaos.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <span className="is-size-7"> NN</span>
-                        </a>
-                    </p>
-                </div>
-                <div className="level-item">
-                    <button
-                        className="link button is-black has-text-danger"
-                        onClick={this.impresium}
-                    >
-                        <span className="is-size-7">Impresium</span>
-                    </button>
-                </div>
-                <div className="level-item">
-                    <button
-                        className="link button is-black has-text-danger"
-                        onClick={this.Datenschutz}
-                    >
-                        <span className="is-size-7">Datenschutz</span>
-                    </button>
-                </div>
-                <div className="level-item">
-                    <button
-                        className="link button is-black has-text-danger"
-                        onClick={this.agb}
-                    >
-                        <span className="is-size-7">AGB Training</span>
-                    </button>
-                </div>
-            </nav>
-        );
-    }
+  impresium = () => {
+    window.open(
+      "http://www.tennis-fuechse-marzahn.de/images/verein-impressum.pdf"
+    );
+  };
+  Datenschutz = () => {
+    window.open("http://www.tennis-fuechse-marzahn.de/images/datenschutz.pdf");
+  };
+  agb = () => {
+    window.open("http://www.tennis-fuechse-marzahn.de/images/agb_training.pdf");
+  };
+  render() {
+    return (
+      <nav className="level navbar is-black is-fixed-bottom is-hidden-mobile">
+        <div className="level-item has-text-centered">
+          <p className="link has-text-white is-size-7">
+            Copyright © 2018 T.S.V. Marzahner Füchse e.V. | Created by
+            <a
+              className="link has-text-danger"
+              href="https://nikolaounkl.github.io/me/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="is-size-7"> NN</span>
+            </a>
+          </p>
+        </div>
+        <div className="level-item">
+          <button
+            className="link button is-black has-text-danger"
+            onClick={this.impresium}
+          >
+            <span className="is-size-7">Impresium</span>
+          </button>
+        </div>
+        <div className="level-item">
+          <button
+            className="link button is-black has-text-danger"
+            onClick={this.Datenschutz}
+          >
+            <span className="is-size-7">Datenschutz</span>
+          </button>
+        </div>
+        <div className="level-item">
+          <button
+            className="link button is-black has-text-danger"
+            onClick={this.agb}
+          >
+            <span className="is-size-7">AGB Training</span>
+          </button>
+        </div>
+      </nav>
+    );
+  }
 }
--- src/components/pages/Vorstand.js
+++ src/components/pages/Vorstand.js
@@ -1,218 +1,86 @@
 import React from "react";
 
 export default function Vorstand() {
-    return (
-        <div className="hero is-danger is-fullheight" id="vorstand">
-            <div className="overlay is-fullheight is-overlay" />
-            <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h1 className="title is-4">Vorstand</h1>
-                    <div className="columns">
-                        <div className="column">
-                            <div className="card has-background-black has-text-white">
-                                <div className="card-content has-text-justified">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-64x64">
-                                                <img
-                                                    src="/images/vorstand3.jpg"
-                                                    alt="vorstand1"
-                                                />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-5 has-text-white">
-                                                Anne Geier
-                                            </p>
-                                            <p className="subtitle is-6 has-text-danger">
-                                                Abteilungsleiterin
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-envelope" />
-                                            </span>
-                                            <span>anne.geier@aol.com</span>
-                                        </div>
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-phone" />
-                                            </span>
-                                            <span>(0151) 25286830</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                            <div className="card has-background-black has-text-white">
-                                <div className="card-content has-text-justified">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-64x64">
-                                                <img
-                                                    src="/images/vorstand1.jpg"
-                                                    alt="vorstand1"
-                                                />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-5 has-text-white">
-                                                Mathias Conrad
-                                            </p>
-                                            <p className="subtitle is-6 has-text-danger">
-                                                Stellvertretender
-                                                Abteilungsleiter
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-envelope" />
-                                            </span>
-                                            <span>aglaval@web.de</span>
-                                        </div>
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-phone" />
-                                            </span>
-                                            <span>(0163) 7451168</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                            <div className="card has-background-black has-text-white">
-                                <div className="card-content has-text-justified">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-64x64">
-                                                <img
-                                                    src="/images/vorstand5.jpeg"
-                                                    alt="vorstand1"
-                                                />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-5 has-text-white">
-                                                Stefan Zande
-                                            </p>
-                                            <p className="subtitle is-6 has-text-danger">
-                                                Kassenwart
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-envelope" />
-                                            </span>
-                                            <span>
-                                                kissenger2000@hotmail.com
-                                            </span>
-                                        </div>
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-phone" />
-                                            </span>
-                                            <span>(0175) 9606384</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div className="hero is-danger is-fullheight" id="vorstand">
+      <div className="overlay is-fullheight is-overlay" />
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title is-4">Vorstand</h1>
+          <div className="columns">
+            <div className="column">
+              <div className="card has-background-black has-text-white">
+                <div className="card-content has-text-justified">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-64x64">
+                        <img src="/images/vorstand2.jpg" alt="vorstand1" />
+                      </figure>
                     </div>
-                    <div className="columns">
-                        <div className="column is-2" />
-                        <div className="column">
-                            <div className="card has-background-black has-text-white">
-                                <div className="card-content has-text-justified">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-64x64">
-                                                <img
-                                                    src="/images/vorstand2.jpg"
-                                                    alt="vorstand1"
-                                                />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-5 has-text-white">
-                                                Konstantin Kleim
-                                            </p>
-                                            <p className="subtitle is-6 has-text-danger">
-                                                Sportwart und Cheftrainer
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-envelope" />
-                                            </span>
-                                            <span>kleim91@gmx.de</span>
-                                        </div>
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-phone" />
-                                            </span>
-                                            <span>(0176) 32453870</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                            <div className="card has-background-black has-text-white">
-                                <div className="card-content has-text-justified">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-64x64">
-                                                <img
-                                                    src="/images/vorstand4.jpg"
-                                                    alt="vorstand1"
-                                                />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-5 has-text-white">
-                                                Denis Kleim
-                                            </p>
-                                            <p className="subtitle is-6 has-text-danger">
-                                                Jugendwart und Trainer
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="content">
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-envelope" />
-                                            </span>
-                                            <span>d.kleim97@gmx.de</span>
-                                        </div>
-                                        <div>
-                                            <span className="icon">
-                                                <i className="fas fa-phone" />
-                                            </span>
-                                            <span>(0176) 84632180</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column is-2" />
+                    <div className="media-content">
+                      <p className="title is-5 has-text-white">
+                        Konstantin Kleim
+                      </p>
+                      <p className="subtitle is-6 has-text-danger">
+                        Abteilungsleiter und Cheftrainer
+                      </p>
                     </div>
+                  </div>
+
+                  <div className="content">
+                    <div>
+                      <span className="icon">
+                        <i className="fas fa-envelope" />
+                      </span>
+                      <span>kleim91@gmx.de</span>
+                    </div>
+                    <div>
+                      <span className="icon">
+                        <i className="fas fa-phone" />
+                      </span>
+                      <span>(0176) 32453870</span>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div className="column">
+              <div className="card has-background-black has-text-white">
+                <div className="card-content has-text-justified">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-64x64">
+                        <img src="/images/vorstand4.jpg" alt="vorstand1" />
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-5 has-text-white">Denis Kleim</p>
+                      <p className="subtitle is-6 has-text-danger">
+                        Stellvertretender Abteilungsleiter und Trainer
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="content">
+                    <div>
+                      <span className="icon">
+                        <i className="fas fa-envelope" />
+                      </span>
+                      <span>d.kleim97@gmx.de</span>
+                    </div>
+                    <div>
+                      <span className="icon">
+                        <i className="fas fa-phone" />
+                      </span>
+                      <span>(0176) 84632180</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
